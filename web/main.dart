2279fe082bdf470e543c2ac5b09701d8df68c427
mqduck <mqduck@mqduck.net>
2019-07-24 19:54:23 -0700

    Add a blank canvas and stuff. Oh boy!

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,25 @@
 import 'dart:html';
 
+import 'package:hyperspace/hyperspace.dart';
+
+final CanvasElement canvas = querySelector('#hypercanvas');
+final CanvasRenderingContext2D ctx = canvas.getContext('2d');
+final DivElement outputDiv = querySelector('#output');
+
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  output('Welcome to Hyperspace!');
+
+  dimensions = 3;
+
+  final cube = HyperObject.hypercube(100.0);
+  cube.update(0);
+
+  for (int i = 0; i < cube.length; ++i) {
+    final edge = cube[i];
+    output('${edge.a} --- ${edge.b}');
+  }
+}
+
+void output(String str) {
+  outputDiv.text += '$str\n';
 }
