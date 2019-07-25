a8b065bc4d969d3c5e54102ace308369aa120d1a
mqduck <mqduck@mqduck.net>
2019-07-25 12:46:21 -0700

    Update for hyperspace library updates.

--- web/main.dart
+++ web/main.dart
@@ -10,27 +10,47 @@ final DivElement outputDiv = querySelector('#output');
 void main() {
   output('Welcome to Hyperspace!');
 
-  dimensions = 4;
-  HyperObject.setDisplayCenter(canvas.width >> 1 as double, canvas.height >> 1 as double);
+//  Hyperspace.dimensions = 3;
+//  Hyperspace.setViewerPosition(canvas.width >> 1 as double, canvas.height >> 1 as double, 8000.0);
+//  Hyperspace.setHyperdimensionDistance(1000.0);
   ctx.strokeStyle = '#FFFFFF';
 
-  final cube = HyperObject.hypercube(100.0);
-  cube.setRotation(0, 2, pi / 6.0);
-  cube.setRotation(1, 2, pi / 4.0);
-  cube.setRotation(1, 3, pi / 1000.0);
-  cube.update(1);
+  final space = Hyperspace(3);
+  space.setViewerPosition(canvas.width >> 1 as double, canvas.height >> 1 as double, 8000.0);
+  space.setHyperdimensionDistance(1000.0);
 
-  ctx.beginPath();
-  for (int i = 0; i < cube.length; ++i) {
-    drawEdge(cube[i]);
-  }
+  final cube = space.addHypercube(100.0);
+  cube.setRotationVelocity(0, 2, pi / 6.0);
+  cube.setRotationVelocity(1, 2, pi / 4.0);
+  space.update(1);
+  drawObject(cube);
+
+  cube.setRotationVelocity(0, 2, 0);
+  cube.setRotationVelocity(1, 2, 0);
+  cube.translateFromList([150.0, 150.0, 0.0, 0.0]);
+  space.update(1);
+  drawObject(cube);
+
+  cube.translateFromList([150.0, 150.0, 0.0, 0.0]);
+  space.update(1);
+  drawObject(cube);
 }
 
-void drawEdge(Edge edge) {
-  output('${edge.a} --- ${edge.b}');
-  ctx.moveTo(edge.a.x, edge.a.y);
-  ctx.lineTo(edge.b.x, edge.b.y);
-  ctx.stroke();
+void drawObject(HyperObject object) {
+  ctx.beginPath();
+  for (int i = 0; i < object.length; ++i) {
+    final edge = object[i];
+    if (edge.a.isVisible && edge.b.isVisible) {
+      output('$edge');
+      output('visible');
+      ctx.moveTo(edge.a.x, edge.a.y);
+      ctx.lineTo(edge.b.x, edge.b.y);
+      ctx.stroke();
+    } else {
+      output('$edge');
+      output('invisible');
+    }
+  }
 }
 
 void output(String str) {
