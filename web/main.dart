93bee814933782fd9103e82683b8547db7537616
mqduck <mqduck@mqduck.net>
2019-07-24 21:05:06 -0700

    Canvas draws a static hypercube.

--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:math';
 
 import 'package:hyperspace/hyperspace.dart';
 
@@ -9,15 +10,27 @@ final DivElement outputDiv = querySelector('#output');
 void main() {
   output('Welcome to Hyperspace!');
 
-  dimensions = 3;
+  dimensions = 4;
+  HyperObject.setDisplayCenter(canvas.width >> 1 as double, canvas.height >> 1 as double);
+  ctx.strokeStyle = '#FFFFFF';
 
   final cube = HyperObject.hypercube(100.0);
-  cube.update(0);
+  cube.setRotation(0, 2, pi / 6.0);
+  cube.setRotation(1, 2, pi / 4.0);
+  cube.setRotation(1, 3, pi / 1000.0);
+  cube.update(1);
 
+  ctx.beginPath();
   for (int i = 0; i < cube.length; ++i) {
-    final edge = cube[i];
-    output('${edge.a} --- ${edge.b}');
+    drawEdge(cube[i]);
   }
+}
+
+void drawEdge(Edge edge) {
+  output('${edge.a} --- ${edge.b}');
+  ctx.moveTo(edge.a.x, edge.a.y);
+  ctx.lineTo(edge.b.x, edge.b.y);
+  ctx.stroke();
 }
 
 void output(String str) {
