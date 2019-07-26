bef50762e589b69e9e51e996b9b9d0862cec2f29
mqduck <mqduck@mqduck.net>
2019-07-26 14:30:36 -0700

    Use WebGL for rendering.

--- web/main.dart
+++ web/main.dart
@@ -1,18 +1,40 @@
+//import 'dart:html';
+//
+//import 'space_view.dart';
+//
+//final CanvasElement canvas = querySelector('#hypercanvas');
+//final CanvasRenderingContext2D ctx = canvas.getContext('2d');
+////final output = (String str) => querySelector('#output').text += '$str\n';
+//final output = (String str) => print(str);
+//SpaceView spaceView;
+//
+//void main() {
+//  ctx.strokeStyle = '#FFFFFF';
+//
+//  spaceView =
+//      SpaceView(4, 800.0, 100.0, canvas, ctx, output: output);
+//  spaceView.targetFrameTime = 16;
+//  final cube = spaceView.space.addHypercube(100.0);
+//  cube.setRotationVelocity(0, 2, 0.00062831853071795865);
+//  cube.setRotationVelocity(0, 1, 0.00062831853071795865);
+//  spaceView.run();
+//}
+
+library webglapp;
+
 import 'dart:html';
+import 'dart:web_gl';
 
 import 'space_view.dart';
 
 final CanvasElement canvas = querySelector('#hypercanvas');
-final CanvasRenderingContext2D ctx = canvas.getContext('2d');
-//final output = (String str) => querySelector('#output').text += '$str\n';
-final output = (String str) => print(str);
+final RenderingContext gl = canvas.getContext3d();
+final output = (String str) => querySelector('#output').text += '$str\n';
+//final output = (String str) => print(str);
 SpaceView spaceView;
 
 void main() {
-  ctx.strokeStyle = '#FFFFFF';
-
-  spaceView =
-      SpaceView(4, 800.0, 100.0, canvas, ctx, output: output);
+  spaceView = SpaceView(4, 800.0, 100.0, canvas, gl, output: output);
   spaceView.targetFrameTime = 16;
   final cube = spaceView.space.addHypercube(100.0);
   cube.setRotationVelocity(0, 2, 0.00062831853071795865);
--- web/space_view.dart
+++ web/space_view.dart
@@ -1,43 +1,112 @@
 import 'dart:async';
 import 'dart:html';
+import 'dart:typed_data';
+import 'dart:web_gl';
 
 import 'package:hyperspace/hyperspace.dart';
 
 class SpaceView {
   final Hyperspace space;
   final CanvasElement canvas;
-  final CanvasRenderingContext2D ctx;
+  final RenderingContext gl;
   final Function(String) output;
   int targetFrameTime = 27; // TODO: modify this based on user's device type (PC, mobile, etc.).
   double _lastTimeStamp = 0.0;
   int _frameCounter = 80;
   var _dragging = false;
   Point<num> mousePosition;
+  double scaleX, scaleY;
+  final Program program;
+  final Buffer vbo;
 
   static _nullOutput(String _) => {};
 
-  SpaceView(int dimensions, double viewerDistance, double spaceDistance, this.canvas, this.ctx,
+  static const _vertexShaderSrc = """
+attribute vec2 position;
+
+void main() {
+  gl_Position = vec4(position, 0.0, 1.0);
+}
+""";
+
+  static const _fragmentShaderSrc = """
+precision mediump float;
+
+void main() {
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+""";
+
+  SpaceView(int dimensions, double viewerDistance, double spaceDistance, this.canvas, this.gl,
       {this.output = _nullOutput})
-      : space = Hyperspace(dimensions) {
-    space.setViewerPosition(canvas.width >> 1 as double, canvas.height >> 1 as double, viewerDistance);
+      : space = Hyperspace(dimensions),
+        program = gl.createProgram(),
+        vbo = gl.createBuffer() {
+    space.setViewerPosition(0.0, 0.0, viewerDistance);
     space.setHyperdimensionDistance(spaceDistance);
+
+    scaleX = 1.0 / (canvas.width >> 1);
+    scaleY = 1.0 / (canvas.height >> 1);
 
     canvas.addEventListener('mousedown', (e) => mouseDown(e as MouseEvent));
     canvas.addEventListener('mouseup', (e) => mouseUp(e as MouseEvent));
     canvas.addEventListener('mouseout', (e) => mouseUp(e as MouseEvent));
     canvas.addEventListener('mousemove', (e) => mouseMove(e as MouseEvent));
 
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    // Compile shaders and link
+    final Shader vs = gl.createShader(WebGL.VERTEX_SHADER);
+    gl.shaderSource(vs, _vertexShaderSrc);
+    gl.compileShader(vs);
+
+    Shader fs = gl.createShader(WebGL.FRAGMENT_SHADER);
+    gl.shaderSource(fs, _fragmentShaderSrc);
+    gl.compileShader(fs);
+
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    // Check if shaders were compiled properly
+    if (!gl.getShaderParameter(vs, WebGL.COMPILE_STATUS)) {
+      output(gl.getShaderInfoLog(vs));
+    }
+
+    if (!gl.getShaderParameter(fs, WebGL.COMPILE_STATUS)) {
+      output(gl.getShaderInfoLog(fs));
+    }
+
+    if (!gl.getProgramParameter(program, WebGL.LINK_STATUS)) {
+      output(gl.getProgramInfoLog(program));
+    }
+
+    if (gl == null) {
+      output("Your browser doesn't seem to support WebGL.");
+      return;
+    }
+
+    gl.bindBuffer(WebGL.ARRAY_BUFFER, vbo);
+    gl.bufferData(
+        WebGL.ARRAY_BUFFER, Float32List(10000), WebGL.DYNAMIC_DRAW); // TODO: Don't just pass a really big list
+
+    int posAttrib = gl.getAttribLocation(program, "position");
+    gl.enableVertexAttribArray(0);
+    gl.vertexAttribPointer(posAttrib, 2, WebGL.FLOAT, false, 0, 0);
+
     output('Welcome to Hyperspace!');
   }
 
   void mouseDown(MouseEvent mouseEvent) {
-//    print('${mouseEvent.client.x}, ${mouseEvent.client.y}');
+//    output('${mouseEvent.client.x}, ${mouseEvent.client.y}');
     mousePosition = mouseEvent.client;
     _dragging = true;
   }
 
   void mouseUp(MouseEvent mouseEvent) {
-//    print('${mouseEvent.client.x}, ${mouseEvent.client.y}');
+//    output('${mouseEvent.client.x}, ${mouseEvent.client.y}');
     _dragging = false;
   }
 
@@ -45,7 +114,7 @@ class SpaceView {
     if (_dragging) {
       final newPosition = mouseEvent.client;
       final diff = newPosition - mousePosition;
-      space.translate(Vector.fromList(space, [diff.x, diff.y]));
+      space.translate(Vector.fromList(space, [diff.x, -diff.y]));
       mousePosition = newPosition;
     }
   }
@@ -58,8 +127,7 @@ class SpaceView {
       _lastTimeStamp = delta;
       space.update(diff);
       redraw();
-      ++_frameCounter;
-      if (_frameCounter == 100) {
+      if (_frameCounter == 10000) {
         output('${1000.0 / diff} FPS');
 //      output('diff = $diff');
         _frameCounter = 0;
@@ -68,42 +136,19 @@ class SpaceView {
     run();
   }
 
-  void redraw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  redraw() {
+    final vertices = List<double>();
     for (final object in space.objects) {
-      ctx.beginPath();
       for (int i = 0; i < object.length; ++i) {
         final edge = object[i];
         if (edge.a.isVisible && edge.b.isVisible) {
-//          output('$edge');
-//          output('visible');
-          ctx.moveTo(edge.a.x, edge.a.y);
-          ctx.lineTo(edge.b.x, edge.b.y);
-          ctx.stroke();
-        } else {
-//          output('$edge');
-//          output('invisible');
+          vertices.addAll([edge.a.x * scaleX, edge.a.y * scaleY, edge.b.x * scaleX, edge.b.y * scaleY]);
         }
       }
     }
-  }
 
-  void testDraw(CanvasRenderingContext2D testctx) {
-    for (final object in space.objects) {
-      testctx.beginPath();
-      for (int i = 0; i < object.length; ++i) {
-        final edge = object[i];
-        if (edge.a.isVisible && edge.b.isVisible) {
-          output('$edge');
-          output('visible');
-          testctx.moveTo(edge.a.x, edge.a.y);
-          testctx.lineTo(edge.b.x, edge.b.y);
-          testctx.stroke();
-        } else {
-          output('$edge');
-          output('invisible');
-        }
-      }
-    }
+    gl.clear(WebGL.COLOR_BUFFER_BIT);
+    gl.bufferSubData(WebGL.ARRAY_BUFFER, 0, Float32List.fromList(vertices));
+    gl.drawArrays(WebGL.LINES, 0, vertices.length >> 1);
   }
 }
