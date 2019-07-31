fe62bb325c98772a87c0cf9fefe4f6be64dbcea2
mqduck <mqduck@mqduck.net>
2019-07-30 21:30:15 -0700

    Switch license to LGPL.

--- lib/hyperspace.dart
+++ lib/hyperspace.dart
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2019 Jeffrey Thomas Piercy
  *
- * This file is part of Hyperspace-Dart.
+ * This file is part of hyperspace.
  *
- * Hyperspace-Dart is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * hyperspace is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU LesserGeneral Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Hyperspace-Dart is distributed in the hope that it will be useful,
+ * hyperspace is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Hyperspace-Dart.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with hyperspace.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /// Hyperspace!
@@ -25,11 +25,7 @@ library hyperspace;
 import 'dart:math';
 
 part 'src/axis_pair_map.dart';
-
 part 'src/hyper_object.dart';
-
 part 'src/hyperspace.dart';
-
 part 'src/transformation_matrix.dart';
-
 part 'src/vector.dart';
