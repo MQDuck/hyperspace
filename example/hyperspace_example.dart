125245063042ec7e601725f86b7f5917f441a64e
mqduck <mqduck@mqduck.net>
2019-07-30 23:35:53 -0700

    Add an actual example to hyperspace_example.dart.

--- example/hyperspace_example.dart
+++ example/hyperspace_example.dart
@@ -1,4 +1,11 @@
+import 'package:hyperspace/hyperspace.dart';
+
 main() {
-//  var awesome = Awesome();
-//  print('awesome: ${awesome.isAwesome}');
+  final space = Hyperspace(4);
+  final cube = space.addHypercube(100.0);
+  cube.setRotationVelocity(0, 3, 1.5);
+  space.update(1);
+  for (int i = 0; i < cube.numEdges; ++i) {
+    print(cube.getEdge(i));
+  }
 }
